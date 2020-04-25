14e1e7cb8dbce76fa78374c51c71fbae63d52bde
Rei Sugiura <reibomaru@g.ecc.u-tokyo.ac.jp>
2020-04-25 23:33:29 +0900

    chartjs_option_pokemonを作成した

---
+++ chartjs_option_pokemon/main.js
@@ -0,0 +1,43 @@
+const pokemonChart = document.getElementById('pokemonChart')
+
+new Chart(pokemonChart, {
+    "type": "radar",
+    "data": {
+        "labels": ["HP", "こうげき", "ぼうぎょ", "すばやさ", "とくこう", "とくぼう"],
+        "datasets": [
+            {
+                "label": "ピカチュウ",
+                "data": [3, 3, 5, 4, 3, 6],
+                "fill": true,
+                "backgroundColor": "rgba(255, 99, 132, 0.2)",
+                "borderColor": "rgb(255, 99, 132)",
+                "pointBackgroundColor": "rgb(255, 99, 132)",
+                "pointBorderColor": "#fff",
+                "pointHoverBackgroundColor": "#fff",
+                "pointHoverBorderColor": "rgb(255, 99, 132)"
+            },
+            {
+                "label": "アチャモ",
+                "data": [4, 5, 6, 3, 3, 4],
+                "fill": true,
+                "backgroundColor": "rgba(54, 162, 235, 0.2)",
+                "borderColor": "rgb(54, 162, 235)",
+                "pointBackgroundColor": "rgb(54, 162, 235)",
+                "pointBorderColor": "#fff",
+                "pointHoverBackgroundColor": "#fff",
+                "pointHoverBorderColor": "rgb(54, 162, 235)",
+                "pointStyle": 'rect'
+            }]
+    },
+    "options": {
+        "elements": {
+            "line": { "tension": 0, "borderWidth": 3 }
+        },
+        scale: {
+            ticks: {
+                suggestedMin: 0,
+                suggestedMax: 6,
+            }
+        }
+    }
+});
--- chartjs_option_temprature/main.js
+++ chartjs_option_temprature/main.js
@@ -1,6 +1,5 @@
 const temperatureChart = document.getElementById('temperatureChart')
 const precipitationChart = document.getElementById('precipitationChart')
-const pokemonChart = document.getElementById('pokemonChart')
 const climateChart = document.getElementById('climateChart')
 
 new Chart(temperatureChart, {
@@ -68,48 +67,6 @@ new Chart(precipitationChart, {
         }
     }
 })
-
-new Chart(pokemonChart, {
-    "type": "radar",
-    "data": {
-        "labels": ["HP", "こうげき", "ぼうぎょ", "すばやさ", "とくこう", "とくぼう"],
-        "datasets": [
-            {
-                "label": "ピカチュウ",
-                "data": [3, 3, 5, 4, 3, 6],
-                "fill": true,
-                "backgroundColor": "rgba(255, 99, 132, 0.2)",
-                "borderColor": "rgb(255, 99, 132)",
-                "pointBackgroundColor": "rgb(255, 99, 132)",
-                "pointBorderColor": "#fff",
-                "pointHoverBackgroundColor": "#fff",
-                "pointHoverBorderColor": "rgb(255, 99, 132)"
-            },
-            {
-                "label": "アチャモ",
-                "data": [4, 5, 6, 3, 3, 4],
-                "fill": true,
-                "backgroundColor": "rgba(54, 162, 235, 0.2)",
-                "borderColor": "rgb(54, 162, 235)",
-                "pointBackgroundColor": "rgb(54, 162, 235)",
-                "pointBorderColor": "#fff",
-                "pointHoverBackgroundColor": "#fff",
-                "pointHoverBorderColor": "rgb(54, 162, 235)",
-                "pointStyle": 'rect'
-            }]
-    },
-    "options": {
-        "elements": {
-            "line": { "tension": 0, "borderWidth": 3 }
-        },
-        scale: {
-            ticks: {
-                suggestedMin: 0,
-                suggestedMax: 6,
-            }
-        }
-    }
-});
 
 new Chart(climateChart, {
     "type": "bar",
